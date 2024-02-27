f92692a9388fc80c9af3252ac6e28d7bb1d29ffe
Walter Arias <walter.arias.aguirre@gmail.com>
2024-02-27 11:44:02 -0500

    subida de imagen al back

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-/* const body = require("body-parser"); */
-/* const bodyParser = require("body-parser"); */
 const app = express();
 const port = 3000;
-/* app.use(bodyParser.json()); */
 app.use(cors());
 app.use(express.json());
 
---
+++ front/assets/js/citizen.js
@@ -0,0 +1,5 @@
+let citizens = document.querySelector("#frmcitizens");
+
+citizens.addEventListener("submit", () => {
+  console.log(citizens[0]);
+});
